d28a308226c76ac8ada6d4049f9e5838fcd39d91
mrizkiaiman <rizki.aiman@arvis.id>
2022-10-28 17:51:19 +0700

    initialize

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  semi: false,
+  printWidth: 140,
+  // endOfLine: 'auto',
+}
---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <p className="text-3xl text-red-500 text-bold">asd</p>
+    </div>
+  )
+}
